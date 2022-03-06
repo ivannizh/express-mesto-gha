236154394b35447c6b34b2bb26888a5d63b65cf1
Ivan Nizhnichenko <ivan_0805@mail.ru>
2022-03-06 13:47:27 +0300

    Minor fixes

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const app = express();
+app.use(logger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,6 +28,11 @@ app.use((req, res, next) => {
 });
 
 app.use('/', router);
+
+app.use((req, res) => {
+  res.status(404);
+  res.json({ message: 'Not found' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
