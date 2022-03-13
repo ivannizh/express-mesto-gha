ffd4e6def2aac895d7f68a9f1a519b96c88e7bcd
Ivan Nizhnichenko <ivan_0805@mail.ru>
2022-03-13 20:34:47 +0300

    Minor fixes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -69,7 +69,7 @@ function login(req, res, next) {
       );
       res.cookie('jwt', token, {
         maxAge: 3600000,
-        httpOnly: true,
+        secure: true,
         domain: 'api.ivannizh.nomoredomains.work',
       });
       res.status(200).send({ message: 'success' });
